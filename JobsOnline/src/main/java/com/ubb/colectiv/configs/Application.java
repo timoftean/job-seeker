ff85718e2e73772396e8dbc40d151e2b4d57b83b
Diana Hatmanu <hatmanudiana@gmail.com>
2017-10-17 21:49:13 +0300

    working now

--- JobsOnline/src/main/java/com/ubb/colectiv/configs/Application.java
+++ JobsOnline/src/main/java/com/ubb/colectiv/configs/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,6 +17,7 @@ import java.util.Arrays;
 @ComponentScan
 @EnableAutoConfiguration
 @EnableJpaRepositories("com.ubb.colectiv.repository")
+@EntityScan("com.ubb.colectiv.entity")
 @SpringBootApplication
 public class Application {
 
