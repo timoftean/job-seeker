0782da7b74059a26f631b86df2d6fc2396b4995b
Nicu <timofte.alexandru05@yahoo.com>
2017-10-22 20:50:46 +0300

    updated project structure

--- src/main/java/com/ubb/colectiv/repository/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ubb.colectiv.repository.user;
-
-import com.ubb.colectiv.entity.user.UserEntity;
-
-public interface UserRepository {
-    UserEntity getUserById(Integer userId);
-}
--- src/main/java/com/ubb/colectiv/service/user/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.ubb.colectiv.service.user;
-
-import com.ubb.colectiv.resource.user.UserInfo;
-
-public interface UserService {
-    UserInfo getUserById(Integer id);
-}
