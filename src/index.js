cab1b71d4006f88b80978fbd0bae98717d8907c4
Nicu <timofte.alexandru05@yahoo.com>
2017-10-23 11:19:12 +0300

    added react-mdl + shown fetched jobs from firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 import './assets/index.css';
 
 ReactDOM.render(
