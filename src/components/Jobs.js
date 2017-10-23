2e8fd53f901f0df9045220e5034b6c1c00c83157
Nicu <timofte.alexandru05@yahoo.com>
2017-10-23 09:53:20 +0300

    created methods to add an retrieve jobs from firebase

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
-
+import { Job } from '../controllers/Job'
 
 export default class Profile extends Component {
-	
+	constructor(props) {
+		super(props)
+		this.job = new Job()
+	}
+	async componentWillMount() {
+		await this.job.saveJob()
+		const jobs = await this.job.getAllJobs()
+		console.log("JOBSS",jobs)
+		
+	}
 	render () {
 		return (
 			<div>
