51595c56280a4298f60568b272322030bc4767e2
Nicu <timofte.alexandru05@yahoo.com>
2017-10-24 12:08:32 +0300

    added profile ui

--- src/components/protected/Profile.js
+++ src/components/protected/Profile.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import {
+	Card,
+	CardTitle,
+	CardText,
+	CardActions,
+	Button,
+	CardMenu,
+	IconButton,
+	List,
+	ListItem,
+  Menu,
+  MenuItem
+} from 'react-mdl'
+
 
 export default class Profile extends Component {
   render () {
     return (
       <div>
-        Profile. This is a protected route. You can only see this if you're authed.
+        <Card shadow={0} style={{width: '500px', margin: 'auto'}}>
+          <CardTitle style={{height: '100px'}}>
+            Profile
+            <div style={{position: 'relative'}}>
+              <IconButton name="more_vert" id="demo-menu-lower-left" />
+              <Menu target="demo-menu-lower-left">
+                <MenuItem>Edit Profile</MenuItem>
+                <MenuItem>Create provider profile</MenuItem>
+                <MenuItem>Post Job</MenuItem>
+              </Menu>
+            </div>
+          </CardTitle>
+          <CardText>
+            <div>
+              <label>Nume </label>
+              <span>Popescu</span>
+            </div>
+            <div>
+              <label>Prenume </label>
+              <span>Andrei</span>
+            </div>
+          </CardText>
+          <CardActions border>
+            <Link to="/jobs" >
+              <Button colored>Show Jobs</Button>
+            </Link>
+            <div className="mdl-layout-spacer"></div>
+            <Link to="/providers" >
+              <Button colored>Show Providers</Button>
+            </Link>
+          </CardActions>
+        </Card>
       </div>
     )
   }
