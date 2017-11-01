28200c3308fe6352a09163f8a06b9b730f77b597
Nicu <timofte.alexandru05@yahoo.com>
2017-11-01 22:18:18 +0200

    refactored PostList

--- src/components/post/PostList.js
+++ src/components/post/PostList.js
@@ -1,40 +1,28 @@
 import React, { Component } from 'react'
 import {List,ListItem,} from 'react-mdl'
 import PostItem from './PostItem'
-import { Post } from '../../controllers/Post'
 
-class PostList extends Component {
-	constructor(props) {
-		super(props)
-		this.postController = new Post()
-	}
-	
-	renderContent = () => {
-		const { posts } = this.props
-		
-		return (
-			<List>
-				{Object.keys(posts).map(key => {
-					console.log("key",posts[key])
-					return (
-						<ListItem key={key}>
-							<PostItem id={key} post={posts[key]} {...this.props}/>
-						</ListItem>)
-					})
-				}
-			</List>
-		)
-	}
-					
-	render () {
-		console.log("postList",this.props.posts)
+const PostList = (props) => {
+		const { posts } = props
 		return (
 			<div>
 				<h1>Posts</h1>
-				{ this.renderContent() }
+				<List>
+					{Object.keys(posts).map(key => {
+						console.log("key",posts[key])
+						return (
+							<ListItem key={key}>
+								<PostItem id={key} post={posts[key]} {...this.props}/>
+							</ListItem>)
+					})}
+				</List>
 			</div>
 		)
 	}
 }
 
 export default PostList
+
+PostList.propTypes = {
+	posts: PropTypes.object
+}
